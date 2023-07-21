49716efcba311fa7e0c11547d069b3b9ba6c542f
ajayfaircent <ajay.prasad@faircent.com>
2023-07-21 18:42:53 +0530

    live config added

--- src/services/api.config.js
+++ src/services/api.config.js
@@ -1,9 +1,11 @@
 // Development Configuration
+// const base_url = "https://fcnode6.faircent.com";
+// const app_id = "865b07d22fab123d4101a11a6e327455";
+// const app_name = "FSIGP";
 
-const base_url = "https://fcnode6.faircent.com";
-const app_id = "865b07d22fab123d4101a11a6e327455";
+// Live Configuration
+const base_url = "https://api.faircent.com";
+const app_id = "fe95241d3fd717dd5377d5b0649e2bf7";
 const app_name = "FSIGP";
 
-
-
-export { base_url, app_id, app_name }
+export { base_url, app_id, app_name };
